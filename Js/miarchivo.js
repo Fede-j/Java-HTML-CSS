6908d1cce3e2ea4a8273c15677f0e08c6a1ba665
Federico Altamirano <fedes227755@gmail.com>
2023-03-19 14:15:07 -0300

    Diseño de tabla

--- Js/miarchivo.js
+++ Js/miarchivo.js
@@ -43,13 +43,14 @@ const cargarRegistro = () => {
 // Recorrer el array "registros" 
 
     registrosParse.forEach((_registros) => {
-      const li = document.createElement ('li')
-      li.innerHTML = `
-      <h4>Nombre:${_registros.Nombre}<h4>
-      <h4>Producto:${_registros.Producto}<h4>
-      <h4>Cantidad:${_registros.Cantidad}<h4>
-      <h4>Precio:${_registros.Precio}<h4>`
-      listaIngresos.append(li) 
+      const tr = document.createElement ('tr')
+      tr.innerHTML = ` 
+        <th scope="row" class="table-success">${_registros.Nombre}</th>
+        <td class="table-success">${_registros.Producto}</td>
+        <td class="table-success">${_registros.Cantidad}</td>
+        <td class="table-success">${_registros.Precio}</td>
+        `
+      listaIngresos.appendChild(tr) 
 });   
 
 
@@ -93,58 +94,6 @@ localStorage.removeItem('registros')
 
 
 
-
-
-// // Funcion constructora
-// class Ingresos {
-//     constructor (material,precio,cantidad) {
-//         this.material =material;
-//         this.precio =precio;
-//         this.cantidad =cantidad;
-//     }
-// }
-
-// // Array de precios
-
-// let precios = [
-//     {material: "Fibra de vidrio", precio: "10"},
-//     {material: "Resina", precio: "15"},
-//     {material: "Catalizador", precio: "12"},
-//     {material: "Gelcoat", precio: "70"},
-// ]
-
-// function cargarIngresos () {
-//     let material = prompt ("Ingrese el material a registrar")
-//     let precio = prompt ("Ingrese el precio")
-//     let cantidad= prompt ("Ingrese cantidad")
-
-//     const nuevoIngreso = new Ingresos (material,precio,cantidad);
-//     arrayRegistro.push(nuevoIngreso);
-//     alert("Ingreso agregado");
-// }
-
-// let arrayRegistro = []
-
-// function verRegistro() {
-//     arrayRegistro.forEach((elemento)=> {
-//         alert ( `Se registro el ingreso de ${elemento.material} por un precio de ${elemento.precio} en la cantidad aprox de ${elemento.cantidad}`)
-//     })
-// }
-
-
-// let opcion = prompt ("Ingrese una opcion: \n 1: Cargar ingreso \n 2:Ver registros \n 3: finalizar") ;
-
-// // bucle while, mientras que..
-
-// while (opcion !== "3") {
-//     if (opcion === "1") {
-//         cargarIngresos(arrayRegistro);
-//     }
-//     if (opcion ==="2") {
-//         verRegistro();
-//     }
-//     opcion = prompt ("Ingrese una opcion: \n 1: Vuelve a Cargar ingreso \n 2:Ver registros \n 3: finalizar")
-// }
 
 
 
